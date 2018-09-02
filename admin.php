5ba6230d9a8baf80530051c0cdac334a8b562da3
benjaminsandras <bensand77@hotmail.fr>
2018-09-02 20:18:14 +0200

    ajout des commentaires

--- admin.php
+++ admin.php
@@ -1,11 +1,8 @@
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
 
-	
+	<meta charset="utf-8">
 	<title>myblog</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
@@ -22,37 +19,44 @@
 		<textarea rows="5" name="newtext"></textarea>
 		<button type="submit">send</button>
 	</form>
+<!-- formulaire d'ajout d'un article -->
 
 	<?php 
 
-	try{
+		try{
 
-		$bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8', 'benji', 'aqwsedcft7777');
-	}
+			$bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8', 'benji', 'aqwsedcft7777');
+		}
 
-	catch (Exception $e){
-		die('Erreur : ' . $e->getMessage());
-	}
+		catch (Exception $e){
+			die('Erreur : ' . $e->getMessage());
+		}
+// connexion base de données
 
-	if (empty($_POST['newtitle']) && empty($_POST['newtext'])) {
-		echo 'var vaut soit 0, vide, ou pas définie du tout';
+		if (empty($_POST['newtitle']) && empty($_POST['newtext'])) {
+			echo 'var vaut soit 0, vide, ou pas définie du tout';
 
-	}
+		}
+// On vérifie le cas où les champs ne seraient pas remplis
 
-	else{
-		echo 'var est définie même si elle est vide';
+		else{
+			echo 'var est définie même si elle est vide';
 		
-		$newtitle = $_POST['newtitle'];
-		$newtext = $_POST['newtext'];
-		
-		
-		$bdd->query("INSERT INTO articles (title, texte)
-			VALUES ('$newtitle', '$newtext')") ;
+			$newtitle = $_POST['newtitle'];
+			$newtext = $_POST['newtext'];
+// On crée deux nouvelles variables auxquelles on associent les valeurs rentrées dans le formulaire d'ajout.	
+// pour $_POST['newtitle']; $_POST fait référence à l'attribut method="post", utilisé dans la balise <form> du formulaire, et ['newtitle'], fait référence à l'attribut name="newtitle", utilisé dans une des deux balise <textarea> (la méthode est la même pour une balise <input>)
 
-		unset($newtitle);
-		unset($newtext);
-		header("location: admin.php");
-	}
+			$bdd->query("INSERT INTO articles (title, texte)
+				VALUES ('$newtitle', '$newtext')") ;
+// requête sql pour insérer les nouvelles variables contenant les valeurs rentrées dans les champs du formulaire d'ajout, dans la table "articles" de la base de données
+
+			unset($newtitle);
+			unset($newtext);
+			header("location: admin.php");
+// On vide les variables et on bloque le rechargement de la page pour empêcher l'envoie automatique du formulaire lors du rechargement de la page
+
+		}
 	
 	?>
 
@@ -61,97 +65,116 @@
 	<div>
 		<?php
 
-		if (isset($_GET['modifier'])) {
-			$id = $_GET['input'];
-			$title = $_GET['title'];
-			$texte = $_GET['texte'];
+			if (isset($_GET['modifier'])) {
+// Première condition qui correspond au click du bouton "modifier" : 
+// si on "appelle" le <button name = "modifier">
+
+				$id = $_GET['input'];
+				$title = $_GET['title'];
+				$texte = $_GET['texte'];
+// On récupère les valeurs des deux textarea et de l'input caché affichés dynamiquement plus bas
 			
-			$query  = "
-			UPDATE
-			articles
-			SET
-			title = '".$title."',
-			texte = '".$texte."'
-			WHERE
-			id    = '".$id."'";
+				$query  = "
+				UPDATE
+				articles
+				SET
+				title = '".$title."',
+				texte = '".$texte."'
+				WHERE
+				id    = '".$id."'";
 
-			/* Envoie de la requête */
-			$bdd->query($query);
+				$bdd->query($query);
 
-			$reponse = $bdd->query('SELECT * FROM articles') ;
+// Requête sql de mise à jour des colonnes "title" et "texte" de la table "articles" avec les valeurs récupérées au-dessus, uniquement pour l'id que l'on a également récupérere au-dessus.
+//j'ai été obligé de mettre ma requête dans une variable et d'appeler ensuite cette variable dans la fonction d'appel de la base de données sinon la requête ne fonctinnait pas, je n'ai pas d'explication à cela...
 
-			while($donnees=$reponse->fetch()){
 
-				echo 
-				'<form method="get">' .
-				'<div class="countain">' .
-				'<textarea rows="2" name="title" class="article" value=' . $donnees['title'] . '>' . $donnees['title'] . '</textarea>' .
-				'<textarea rows="5" name="texte" class="article" value=' . $donnees['texte'] . '>' . $donnees['texte'] . '</textarea>' . 
+				$reponse = $bdd->query('SELECT * FROM articles') ;
+
+// Requête sql de séléction de la table "articles"
+
+				while($donnees=$reponse->fetch()){
+
+					echo 
+					'<form method="get">' .
+					'<div class="countain">' .
+					'<textarea rows="2" name="title" class="article" value=' . $donnees['title'] . '>' . $donnees['title'] . '</textarea>' .
+					'<textarea rows="5" name="texte" class="article" value=' . $donnees['texte'] . '>' . $donnees['texte'] . '</textarea>' . 
 				
-				'<button name="modifier" type="submit" >' . 'modifier' . '</button>' . 
-				'<button name="supp" type="submit">' . 'supprimer' . '</button>' .
-				'<input class="inputid" name="input" value=' . $donnees['id'] . '></form>' .
+					'<button name="modifier" type="submit" >' . 'modifier' . '</button>' . 
+					'<button name="supp" type="submit">' . 'supprimer' . '</button>' .
+					'<input class="inputid" name="input" value=' . $donnees['id'] . '></form>' .
 
-				'</div>' ;
+					'</div>' ;
 
+				}
+// On affiche dynamiquement les données de la table "articles" à l'intérieur de deux textarea - de manière à pouvoir écrire à l'intérieur ensuite et donc de pouvoir "modifier" directement l'article que l'on a sous les yeux.
+// On place ces deux textarea à l'intérieur d'un formulaire d'envoi <form method="get">, dans lequel on crée également deux boutons, <button name="modifier" et <button name="supp",                                     ainsi qu'un input <input value=' . $donnees['id'] . '> dont la valeur sera égale à la valeur de l'id correspondant dans la base de données. On cachera ensuite cet input à l'aide du css.
 
-			}
+// C'est à partir de cet affichage dynamique, et grâce à lui, que l'on peut éffectuer les trois premières étapes à partir de la première condition !!!
 
+			} //fin de la première condition
 
-		}
+			else if (isset($_GET['supp'])) {
+// Deuxième condition qui correspond au click du bouton "supp" : 
+// si on "appelle" le <button name = "supp">
 
-		else if (isset($_GET['supp'])) {
-			$id = $_GET['input'];
+				$id = $_GET['input'];
+// On récupère la valeur de l'input caché
 
-			$bdd->query("DELETE FROM articles WHERE id=".
+				$bdd->query("DELETE FROM articles WHERE id=".
 				$id) ;
+// Requête sql de suppression de la table "articles" uniquement pour la colonne où l'id correspond à la valeur récupérée juste avant
 
 			
-			unset($id);
+				unset($id);
 
-			$reponse = $bdd->query('SELECT * FROM articles') ;
+				$reponse = $bdd->query('SELECT * FROM articles') ;
+// Requête sql de séléction de la table "articles"
 
-			while($donnees=$reponse->fetch()){
+				while($donnees=$reponse->fetch()){
 
-				echo 
-				'<form method="get">' .
-				'<div class="countain">' .
-				'<textarea rows="2" name="title" class="article" value=' . $donnees['title'] . '>' . $donnees['title'] . '</textarea>' .
-				'<textarea rows="5" name="texte" class="article" value=' . $donnees['texte'] . '>' . $donnees['texte'] . '</textarea>' . 
+					echo 
+					'<form method="get">' .
+					'<div class="countain">' .
+					'<textarea rows="2" name="title" class="article" value=' . $donnees['title'] . '>' . $donnees['title'] . '</textarea>' .
+					'<textarea rows="5" name="texte" class="article" value=' . $donnees['texte'] . '>' . $donnees['texte'] . '</textarea>' . 
 				
-				'<button name="modifier" type="submit" >' . 'modifier' . '</button>' . 
-				'<button name="supp" type="submit">' . 'supprimer' . '</button>' .
-				'<input class="inputid" name="input" value=' . $donnees['id'] . '></form>' .
+					'<button name="modifier" type="submit" >' . 'modifier' . '</button>' . 
+					'<button name="supp" type="submit">' . 'supprimer' . '</button>' .
+					'<input class="inputid" name="input" value=' . $donnees['id'] . '></form>' .
 
-				'</div>' ;
+					'</div>' ;
 
+				}
+// Ici l'affichage est exactement le même que pour la première condition, et la logique générale est exactement la même
+
+			} // Fin de la deuxième condition
+
+			else{
+// si l'on a ni "appellé" le <button name = "modifier">, ni le <button name = "supp">, alors :
+
+				$reponse = $bdd->query('SELECT * FROM articles') ;
+// Requête sql de séléction de la table "articles"
+
+				while($donnees=$reponse->fetch()){
+
+					echo 
+					'<form method="get">' .
+					'<div class="countain">' .
+					'<textarea rows="2" name="title" class="article" value=' . $donnees['title'] . '>' . $donnees['title'] . '</textarea>' .
+					'<textarea rows="5" name="texte" class="article" value=' . $donnees['title'] . '>' . $donnees['texte'] . '</textarea>' . 
+				
+					'<button name="modifier" type="submit" >' . 'modifier' . '</button>' . 
+					'<button name="supp" type="submit">' . 'supprimer' . '</button>' .
+					'<input class="inputid" name="input" value=' . $donnees['id'] . '></form>' .
+
+					'</div>' ;
+
+				}
+// Affichage dynammique, toujours inchangé
 
 			}
-
-		}
-
-		else{
-
-			$reponse = $bdd->query('SELECT * FROM articles') ;
-
-			while($donnees=$reponse->fetch()){
-
-				echo 
-				'<form method="get">' .
-				'<div class="countain">' .
-				'<textarea rows="2" name="title" class="article" value=' . $donnees['title'] . '>' . $donnees['title'] . '</textarea>' .
-				'<textarea rows="5" name="texte" class="article" value=' . $donnees['title'] . '>' . $donnees['texte'] . '</textarea>' . 
-				
-				'<button name="modifier" type="submit" >' . 'modifier' . '</button>' . 
-				'<button name="supp" type="submit">' . 'supprimer' . '</button>' .
-				'<input class="inputid" name="input" value=' . $donnees['id'] . '></form>' .
-
-				'</div>' ;
-
-
-			}
-
-		}
 		?>
 
 	</div>
@@ -164,8 +187,7 @@
 </html>
 
 
-<!-- 
-	id=' . uniqid() . ' -->
+
 
 
 
--- blog.php
+++ blog.php
@@ -2,13 +2,47 @@
 <html>
 <head>
 	<title>myblog</title>
+	<link rel="stylesheet" type="text/css" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
-	<div>
-		<h1>My Blog</h1>
-	</div>
+	<nav class="navbar navbar-expand-lg navbar navbar-dark bg-primary">
+		<a class="navbar-brand" href="#">My Blog</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Dropdown
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#">Action</a>
+						<a class="dropdown-item" href="#">Another action</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#">Something else here</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" href="#">Disabled</a>
+				</li>
+			</ul>
+			<form class="form-inline my-2 my-lg-0">
+				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+				<button class="btn btn-dark my-2 my-sm-0" type="submit">Search</button>
+			</form>
+		</div>
+	</nav>
+	<!--navbar bootstrap-->
 
 	<?php 
 
@@ -27,38 +61,53 @@
 
 	}
 	?>
-	<div>
-		<?php
+<!-- connexion à la BDD -->
 
-		$reponse = $bdd->query('SELECT * FROM articles') ;
+	<div class="container">
+		<div class="row">
 
-		while($donnees=$reponse->fetch()){
+			<div class="col-sm-4"></div>
 
-			echo 
+			<div class="col-sm-6">
+				<?php
 
-			'<div class="countain">' .
-			'<div class="article">' . $donnees['title'] . '</div>' .
-			'<div class="article">' . $donnees['texte'] . '</div>' .
-			'</div>' ;
+				$reponse = $bdd->query('SELECT * FROM articles') ;
+// requête sql de toutes les données de la table "articles"
 
+				while($donnees=$reponse->fetch()){
 
-		}
-		?>
+					echo 
 
+					'<div class="countain">' .
+					'<div class="article">' . $donnees['title'] . '</div>' .
+					'<div class="article">' . $donnees['texte'] . '</div>' .
+					'</div>' ;
+// affichage dynamique de la colonne "title" et de la colonne "texte" de la table "articles"
+
+				}
+				?>
+
+			</div>
+
+			<div class="col-sm-2"></div>
+
+		</div>
+		
 	</div>
 
-	
-
-
-
-
-
-
-
-
-
 
 	
+
+
+<!-- page d'interface visiteur du blog -->
+
+
+
+
+
+
+	<script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
+	<script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 
 </body>
 </html>
